0a389ac160ac43201d6d4cbabc65800493425e54
t-tharnigan <tharniganthambibe@gmail.com>
2026-06-15 11:56:22 +0200

    Klasse titel

--- Film/Program.cs
+++ Film/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        var MijnFilm = new Film {
+            Titel = "Inception",
+            Genre = "Sci-fi",
+            Duurtijd = 148
+        };
+
+        Console.WriteLine("Film informatie:");
+        MijnFilm.ToonInfo();
+    }
+}
+
+class Film {
+    public string? Titel;
+    public string? Genre;
+    public int Duurtijd;
+
+    public void ToonInfo() {
+        Console.WriteLine($"Titel: {Titel}, Genre: {Genre}, Duurtijd: {Duurtijd} minuten");
+    }
+}
